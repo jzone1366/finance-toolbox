2d7edf3077bafd6c07674ae1160e26457c69666e
Joshua Zone <jzone1229@gmail.com>
2025-03-28 10:12:16 -0400

    Update to add FeatureFlags and move Navigation to it's own component.

---
+++ src/config/featureFlags.ts
@@ -0,0 +1,4 @@
+export const featureFlags = {
+  showNav: import.meta.env.VITE_FEATURE_FLAG_SHOW_NAV === 'true',
+  showPaycheck: import.meta.env.VITE_FEATURE_FLAG_SHOW_PAYCHECK === 'true',
+}; 
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import {
   createRootRoute,
   createRoute,
   createRouter,
+  redirect,
 } from '@tanstack/react-router'
 import MainLayout from './components/Layout/MainLayout'
 import AmortizationLayout from './components/Amortization/AmortizationLayout'
 import PaycheckCalculator from './components/Paycheck/PaycheckCalculator'
 import NotFound from './components/NotFound'
+import { featureFlags } from './config/featureFlags'
 
 const rootRoute = createRootRoute({
   component: MainLayout,
@@ -21,6 +23,13 @@ const indexRoute = createRoute({
 const paycheckRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: '/paycheck',
+  beforeLoad: () => {
+    if (!featureFlags.showPaycheck) {
+      throw redirect({
+        to: '/',
+      })
+    }
+  },
   component: PaycheckCalculator,
 })
 
